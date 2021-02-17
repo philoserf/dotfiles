3b72b3798a1f9f0f064d783003a828006d958a9f
Mark Ayers <mark@philoserf.com>
2021-02-16 20:44:02 -0500

    Misc updates

--- .init.d/general.sh
+++ .init.d/general.sh
@@ -1,11 +1,14 @@
 # shellcheck disable=SC2148,SC2164
 
-alias be='bundle exec'
 alias k='kubectl'
 alias kctx='kubectx'
+alias kge='kubectl get events'
+alias kgea='kubectl get events --all-namespaces'
+alias kgeaw='kubectl get events --all-namespaces --watch'
+alias kgew='kubectl get events --watch'
 alias kns='kubens'
 alias ll="ls -FrtohpqGl"
 alias lla='ll -A'
 alias update='task --taskfile ${HOME}/Taskfile.yml'
-pushd() { builtin pushd "$@" >/dev/null; }
 popd() { builtin popd "$@" >/dev/null; }
+pushd() { builtin pushd "$@" >/dev/null; }
--- .init.d/rubocop-ready.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck disable=SC2148
-
-rubocop-ready() {
-  touch .rubocop.yml
-  rubocop --auto-correct
-  rubocop --auto-gen-config
-}
